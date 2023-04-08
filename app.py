3054d961eaeef015e3dcdb06a912d9802f91c81a
Preet131 <90319247+Preet131@users.noreply.github.com>
2023-04-08 11:47:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+# Program to Upload Color Image and convert into Black & White image
+import os
+from flask import  Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+# import additional libraries below
+import cv2 
+import numpy as np 
+
+
+
+app = Flask(__name__)
+
+# Open and redirect to default upload webpage
+@app.route('/')
+def load_form():
+    return render_template('upload.html')
+
+# Function to upload image and redirect to new webpage
+@app.route('/gray', methods=['POST'])
+def upload_image():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    # write the read and write function on image below 
+    fileData = make_grayscale(file.read())
+    with open(os.path.join('static/', filename), 'wb') as f:
+        f.write(fileData)        
+
+
+        # ends here
+
+    display_message = 'Image successfully uploaded and displayed below'
+    return render_template('upload.html', filename=filename, message = display_message)
+
+
+# Write the make_grayscale() function below
+def make_grayscale(image):
+    imageArray = np.fromstring(image, dtype='uint8')
+    print("Image Array:", imageArray)
+    decodeArrayToImage = cv2.imdecode(imageArray, cv2.IMREAD_UNCHANGED)
+    print("Decode Values of Image:", decodeArrayToImage)
+
+    convertedGrayImage = cv2.cvtColor(decodeArrayToImage, cv2.COLOR_RGB2GRAY)
+
+    status, outputImage = cv2.imencode('.PNG', convertedGrayImage)
+    print('Status:', status)
+
+    return outputImage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# make_grayscale() function ends above
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename=filename))
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
